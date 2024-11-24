a95fb6c0d317e1cd2d11dd1640a1f5933a4b267c
Manish Kumar <kumarmanish.gkv@gmail.com>
2024-11-24 22:51:30 +0530

    delete some extra file

--- components/Home/Slider.js
+++ components/Home/Slider.js
@@ -15,10 +15,12 @@ export default function SimpleSlider() {
     cssEase: "linear",
   };
 
+  const url = "https://phume.s3.ap-south-1.amazonaws.com/images";
+
   const slides = [
-    { id: 1, title: "Introduce img", img: "/introduce_page.jpg" },
-    { id: 2, title: "Select game img", img: "/select_game.jpg" },
-    { id: 3, title: "Board img", img: "/board.jpg" },
+    { id: 1, title: "Introduce img", img: `${url}/introduce_page.jpg` },
+    { id: 2, title: "Select game img", img: `${url}/select_game.jpg` },
+    { id: 3, title: "Board img", img: `${url}/board.jpg` },
   ];
 
   return (
